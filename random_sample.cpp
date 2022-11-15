50915135673bc4832edf5a798de6e036fd8f12b0
Sravan Sriram <ssrav@MacBook-Pro-2.local>
2022-11-15 15:05:10 -0600

    Random sampling in codelet 2.0

--- random_sample.cpp
+++ random_sample.cpp
@@ -5,12 +5,13 @@ class RandomSampleVertex : public poplar::MultiVertex {
     public:
     // Fields
     poplar::Input<poplar::Vector<int>> local_list;
+    poplar::Input<int> over_sampling_factor;
     poplar::Output<poplar::Vector<int>> sampled_list;
 
     // Compute function
     bool compute(unsigned workerId) {
-        for (std::size_t i = workerId; i < local_list.size(); i += MultiVertex::numWorkers()) {
-            sampled_list[i] = rand();
+        for (std::size_t i = workerId * over_sampling_factor; i < local_list.size(); i += MultiVertex::numWorkers()) {
+            sampled_list.push_back(local_list[i]);
         }
     }
 };
