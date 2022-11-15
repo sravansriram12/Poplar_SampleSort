97125af3fe5310570f1356e56083da4d539fe60f
Sravan Sriram <ssrav@MacBook-Pro-2.local>
2022-11-15 16:42:50 -0600

    Multiworkers

--- random_sample.cpp
+++ random_sample.cpp
@@ -10,7 +10,8 @@ class RandomSampleVertex : public poplar::MultiVertex {
     // Compute function
     bool compute(unsigned workerId) {
         unsigned new_workerId = workerId++;
-        unsigned num_workers = MultiVertex::numWorkers;
+        unsigned num_workers = 0;
+        num_workers += MultiVertex::numWorkers;
         unsigned increment_by = num_workers + 1 + (num_workers * (over_sampling_factor - 1));
         unsigned starting_position = workerId + ((workerId + 1) * (over_sampling_factor - 1));
         for (std::size_t i = starting_position; i < local_list.size(); i += increment_by) {
