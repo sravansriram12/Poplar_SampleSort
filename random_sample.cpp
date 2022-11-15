6768bc6c32f923b0b6ff58aca04378f48cfcd54f
Sravan Sriram <ssrav@MacBook-Pro-2.local>
2022-11-15 12:05:23 -0600

    Fixed random in codelet

--- random_sample.cpp
+++ random_sample.cpp
@@ -1,5 +1,4 @@
 #include <poplar/Vertex.hpp>
-#include <stdlib.h>
 
 class RandomSampleVertex : public poplar::MultiVertex {
     public:
@@ -9,9 +8,8 @@ class RandomSampleVertex : public poplar::MultiVertex {
 
     // Compute function
     bool compute(unsigned workerId) {
-        srand (time(NULL));
         for (std::size_t i = workerId; i < sampled_list.size(); i += MultiVertex::numWorkers()) {
-            sampled_list[i] = rand() % in.size();
+            sampled_list[i] = 2;
         }
         return true;
     }
