db3e351c8e2a454538a5f4b28aedf23013cef943
Sravan Sriram <ssrav@MacBook-Pro-2.local>
2022-11-15 16:40:10 -0600

    Correct operations in codelete 2.0

--- random_sample.cpp
+++ random_sample.cpp
@@ -10,7 +10,8 @@ class RandomSampleVertex : public poplar::MultiVertex {
     // Compute function
     bool compute(unsigned workerId) {
         unsigned new_workerId = workerId++;
-        unsigned increment_by = *MultiVertex::numWorkers + 1 + (*MultiVertex::numWorkers  * (over_sampling_factor - 1));
+        unsigned num_workers = MultiVertex::numWorkers;
+        unsigned increment_by = num_workers + 1 + (num_workers * (over_sampling_factor - 1));
         unsigned starting_position = workerId + ((workerId + 1) * (over_sampling_factor - 1));
         for (std::size_t i = starting_position; i < local_list.size(); i += increment_by) {
             unsigned output_index = ((i + 1) / over_sampling_factor) - 1;
