bfcb9882e3ceac04c488f7e9a7de890ceb1b0733
Sravan Sriram <ssrav@MacBook-Pro-2.local>
2022-11-21 17:59:26 -0600

    Check 25

--- main.cpp
+++ main.cpp
@@ -169,6 +169,7 @@ int main() {
   // Get back buckets from remote buffer
   // Do rest of the processing
   int* last_index;
+  cout << buckets.shape().size() << endl;
   buckets[0][0].getConstantValue(last_index);
   int index = *last_index;
   cout << index << endl; 
