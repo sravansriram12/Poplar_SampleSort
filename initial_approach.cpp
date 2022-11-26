28ecae7a1bfff3e0debecefb34568b63246d04c4
Sravan Sriram <ssrav@MacBook-Pro-2.lan>
2022-11-26 14:48:57 -0600

    Trying initial approach 16.0

--- initial_approach.cpp
+++ initial_approach.cpp
@@ -173,9 +173,9 @@ int main() {
         graph.setTileMapping(initial_list[i][j], processor_list[idx]);
         cout << "here" << endl;
         if (j == 0) {
-            current_processor_list = initial_list.slice({i, i + 1}, {j, j + 1}).reshape({1});
+            current_processor_list = initial_list.slice({i, j}, {i, j + 1}).reshape({1});
         } else {
-            current_processor_list = concat(current_processor_list, initial_list.slice({i, i + 1}, {j, j + 1}).reshape({1}));
+            current_processor_list = concat(current_processor_list, initial_list.slice({i, j}, {i, j + 1}).reshape({1}));
         }
         cout << "here" << endl;
         idx++;
