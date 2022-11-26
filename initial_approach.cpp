a5fbef45ebdf55cfedc79d0fef8491d345f782db
Sravan Sriram <ssrav@MacBook-Pro-2.lan>
2022-11-26 16:19:31 -0600

    Trying initial approach 45.0

--- initial_approach.cpp
+++ initial_approach.cpp
@@ -195,7 +195,7 @@ int main() {
   cout << "here" << endl;
   for (unsigned i = 0; i < p; i++) {
     ArrayRef<Tensor> tensor = final_processor_sort(local_sort, graph, initial_list, indexes[i], i);
-    cout << tensor[0].shape() << endl;
+    cout << tensor[0].numElements() << endl;
    
   }
   
