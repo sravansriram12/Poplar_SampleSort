369dd64bec36f19de0fd028fa008dfa2c24ba988
Sravan Sriram <ssrav@MacBook-Pro-2.lan>
2022-11-26 18:48:02 -0600

    On IPU hardware 12.0

--- initial_approach.cpp
+++ initial_approach.cpp
@@ -69,9 +69,9 @@ void find_processor(ComputeSet& computeSet, Graph& graph, Tensor input_list, Ten
 int main() {
   // Create the IPU model device
 
-  unsigned n = 4000000;  // number of elements
-  unsigned p = 300;   // number of processors (tiles)
-  unsigned k = 200;
+  unsigned n = 20000000;  // number of elements
+  unsigned p = 500;   // number of processors (tiles)
+  unsigned k = 1000;
   unsigned local_list_size = n / p;
   const char *dev = "model-ipu2";
   srand (time(NULL));
