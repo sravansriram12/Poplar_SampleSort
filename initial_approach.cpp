47284169dc9f01d801d4615935a4440db9cec04b
Sravan Sriram <ssrav@MacBook-Pro-2.lan>
2022-11-26 18:22:31 -0600

    Check timing 2.0

--- initial_approach.cpp
+++ initial_approach.cpp
@@ -66,9 +66,9 @@ void find_processor(ComputeSet& computeSet, Graph& graph, Tensor input_list, Ten
 int main() {
   // Create the IPU model device
 
-  unsigned n = 20000;  // number of elements
+  unsigned n = 200000;  // number of elements
   unsigned p = 100;   // number of processors (tiles)
-  unsigned k = 20;
+  unsigned k = 50;
   unsigned local_list_size = n / p;
   const char *dev = "model-ipu2";
   srand (time(NULL));
