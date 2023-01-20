38b79b6848d30458e3ef76357d2babef0fee8594
Sravan Sriram <ssrav@MacBook-Pro-2.local>
2023-01-20 11:02:22 -0600

    Made quick sort changes 10.0

--- vertices.cpp
+++ vertices.cpp
@@ -88,7 +88,7 @@ class Samples : public MultiVertex {
     Output<Vector<int>> local_samples;
 
     bool compute(unsigned workerId) {
-      unsigned starting_position = (local_sorted_list.size() / factor) * (workerId + 1);
+      unsigned starting_position = (local_sorted_list.size() / factor) * (workerId + 1) - 1;
       unsigned increment_by = MultiVertex::numWorkers() * (local_sorted_list.size() / factor);
       for (unsigned i = starting_position; i < local_sorted_list.size(); i += increment_by) {
         unsigned output_index = (i / (local_sorted_list.size() / factor)) - 1;
