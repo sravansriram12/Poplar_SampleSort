8006886076552307b97db29ed1ccddfd45a35836
Sravan Sriram <ssrav@MacBook-Pro-2.local>
2023-01-20 10:26:39 -0600

    Made quick sort changes 2.0

--- vertices.cpp
+++ vertices.cpp
@@ -12,35 +12,32 @@ class QuickSort : public Vertex {
         local_list[B] = temp;
     }
 
-    int median_three(int a, int b, int c) {
-        if ((a > b) ^ (a > c)) 
-            return a;
-        else if ((b < a) ^ (b < c)) 
-            return b;
-        else
-            return c;
+    int median_three(int low, int high) {
+       unsigned mid = low + (high - low) / 2;
+       if (a[high] < a[low]) exchange(low, high);
+       if (a[mid] < a[low]) exchange(low, mid);
+       if (a[high] < a[mid]) exchange(mid, high);
+       return mid;
     }
 
     int partition(int low, int high) {
-        int pivot = local_list[low]; // pivot
-        int i = (low - 1);
-    
-        for (int j = low; j <= high - 1; j++) {
-            if (local_list[j] < pivot) {
-                i++;
-                int temp = local_list[i];
-                local_list[i] = local_list[j];
-                local_list[j] = temp;
+        unsigned i = low, j = high + 1;
+        int pivot = local_list[low];
+        while(1) {
+            while (a[++i] < pivot) {
+                if (i == high) break;
             }
+            while (pivot < a[--j]) {
+                if (j == low) break;
+            }
+            if (i >= j) break;
+            exchange(i, j);
         }
-        int temp = local_list[i + 1];
-        local_list[i + 1] = local_list[high];
-        local_list[high] = temp;
-        return (i + 1);
+        exchange(low, j);
+        return j;
     }
   
     void quickSort(int low, int high) {
-
         if (low < high) {
             int median = median_three(local_list[low], local_list[(low + high) / 2], local_list[high]);
             exchange(low, median);
