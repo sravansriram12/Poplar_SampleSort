44d35983b17bf9f870e819f8b2efa71f3b68b1d4
Sravan Sriram <sravansriram12@gmail.com>
2023-03-27 12:14:41 -0500

    Updated merge sorting 31.0

--- odd_even_mergesort.cpp
+++ odd_even_mergesort.cpp
@@ -145,7 +145,7 @@ int main(int argc, char *argv[]) {
     
     for (int k = 0; k < p_in_use; k++) {
         prog.add(Execute(cs_even));
-        prog.add(padding[0]);
+        prog.add(paddings[0]);
         prog.add(Execute(cs_odd));
     }
 
