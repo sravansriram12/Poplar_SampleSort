4eaccbf3b6a447a21aab7f251a3bc9dd81db49e8
Sravan Sriram <sravansriram12@gmail.com>
2023-03-27 12:32:49 -0500

    Updated merge sorting 40.0

--- odd_even_mergesort.cpp
+++ odd_even_mergesort.cpp
@@ -142,8 +142,8 @@ int main(int argc, char *argv[]) {
         nums2 += (numbers_per_tile * 2);
     }
 
-    prog.add(PrintTensor(initial_list);
-    prog.add(PrintTensor(initial_list);
+    prog.add(PrintTensor(initial_list));
+    prog.add(PrintTensor(initial_list));
 
     
     for (int k = 0; k < p_in_use; k++) {
