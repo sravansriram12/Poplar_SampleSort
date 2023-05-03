37cfbb6cacf4b57da3a0a8f08d82cf72d800ac67
Sravan Sriram <sravansriram12@gmail.com>
2023-05-03 07:28:58 -0500

    binary merge sort 9.0

--- odd_even_mergesort.cpp
+++ odd_even_mergesort.cpp
@@ -115,7 +115,7 @@ int main(int argc, char *argv[]) {
     }
   
     prog.add(Execute(cs));
-
+    nums = 0;
      for(int i = 0; i < p_in_use; i++) {
         int end_index = std::min(n, nums + numbers_per_tile);
         prog.add(WriteUndef(initial_list.slice(nums + k + 1, end_index)));
